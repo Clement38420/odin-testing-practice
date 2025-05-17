8ad271b46c69b2397c4dcad82882f156e2841e26
Clement Charbonnel <c.charbo38@gmail.com>
2025-05-17 12:26:01 +0000

    EMPTY STRING CAPITALIZE

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export function capitalize(string) {
+  if (!string) return "";
   return string.at(0).toUpperCase() + string.substring(1);
 }
--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ import { capitalize } from "./index.js";
 it("Happy path", () => {
   expect(capitalize("bonjour")).toBe("Bonjour");
 });
+
+it("Empty", () => {
+  expect(capitalize("")).toBe("");
+});
