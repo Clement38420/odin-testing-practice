a31f22d2a8b0aa596f3b8a82dd424146def00a99
Clement Charbonnel <c.charbo38@gmail.com>
2025-05-18 18:49:10 +0200

    HAPPY PATH REVERSE STRING

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ export function capitalize(string) {
   if (!string) return "";
   return string.at(0).toUpperCase() + string.substring(1);
 }
+
+export function reverseString(string) {
+  let reversedString = "";
+  for (let i = 1; i <= string.length; i++) {
+    reversedString += string.charAt(string.length - i);
+  }
+  return reversedString;
+}
--- index.test.js
+++ index.test.js
@@ -1,9 +1,17 @@
-import { capitalize } from "./index.js";
+import { capitalize, reverseString } from "./index.js";
 
-it("Happy path", () => {
-  expect(capitalize("bonjour")).toBe("Bonjour");
+describe("Capitalize", () => {
+  it("Happy path", () => {
+    expect(capitalize("bonjour")).toBe("Bonjour");
+  });
+
+  it("Empty", () => {
+    expect(capitalize("")).toBe("");
+  });
 });
 
-it("Empty", () => {
-  expect(capitalize("")).toBe("");
+describe("reverseString", () => {
+  it("Happy path", () => {
+    expect(reverseString("bonjour")).toBe("ruojnob");
+  });
 });
